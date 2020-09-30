f78ed6127b2dc83aa4236f1e427ebcf8b8aedbae
tt12b <tt12b89@gmail.com>
2020-09-30 18:35:45 +0900

    커밋_1

---
+++ bootStudy/src/main/java/com/bootStudy/web/bootStudy/BootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.bootStudy.web.bootStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStudyApplication.class, args);
+	}
+
+}
